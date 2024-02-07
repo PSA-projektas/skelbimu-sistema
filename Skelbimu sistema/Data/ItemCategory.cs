4513f9e2a5ceb538667d19949dcaff9edbfa972c
Aivaras Mickus <aivaras.mickus@ktu.edu>
2024-02-07 20:55:14 +0200

    added model item and started wokrking on home page

---
+++ Skelbimu sistema/Data/ItemCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace Skelbimu_sistema.Data
+{
+	public enum ItemCategory
+	{
+		Motherboard,
+		CPU,
+		GPU,
+		RAM,
+		Storage,
+		PSU,
+		Case,
+		Cooling,
+		Peripherals,
+		Other // Nezinau ar kita elektronika rasyt bet jei ka dadesim
+	}
+}
---
+++ Skelbimu sistema/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using Skelbimu_sistema.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Skelbimu_sistema.Models
+{
+	public class Item
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public double Price { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		public string StartDate { get; set; }
+
+		public string EndDate { get; set; }
+
+		public ItemCategory ItemCategory { get; set; }
+
+	}
+}
