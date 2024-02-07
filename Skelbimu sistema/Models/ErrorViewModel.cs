ec2d0deee3bb0e5a3c43c40ee1f0e783a076fc3b
Aivaras Mickus <aivaras.mickus@ktu.edu>
2024-02-07 19:45:55 +0200

    Add project files.

---
+++ Skelbimu sistema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Skelbimu_sistema.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
