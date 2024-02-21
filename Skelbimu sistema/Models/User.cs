2ff44f29a5765d1a781076660372ef739d44443c
lukvas <lukas.vasiliauskas@ktu.edu>
2024-02-21 20:27:28 +0200

    Pridėtas prisijungimo puslapis

--- Skelbimu sistema/Models/User.cs
+++ Skelbimu sistema/Models/User.cs
@@ -6,6 +6,8 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+
+        public string Password { get; set; }
         public bool Blocked { get; set; }
 
     }
@@ -17,11 +19,10 @@
             // Generate mock user data
             var users = new List<User>
                 {
-                    new User { Id = 1, FirstName = "Steponas", LastName="Kairys", Email = "pastas@example.com", Blocked = false },
-                    new User { Id = 2, FirstName = "Antanas", LastName="Smetona", Email = "pastas@example.com", Blocked = false },
-                    new User { Id = 3, FirstName = "Vladas", LastName="Mironas", Email = "pastas@example.com", Blocked = true },
-                    new User { Id = 4, FirstName = "Jurgis", LastName="Šaulys", Email = "pastas@example.com", Blocked = true },
-                    // Add more mock users as needed
+                    new User { Id = 1, FirstName = "Steponas", LastName="Kairys", Email = "pastas@example.com", Password = "password", Blocked = false },
+                    new User { Id = 2, FirstName = "Antanas", LastName="Smetona", Email = "pastas@example.com", Password = "password", Blocked = false },
+                    new User { Id = 3, FirstName = "Vladas", LastName="Mironas", Email = "pastas@example.com", Password = "password", Blocked = true },
+                    new User { Id = 4, FirstName = "Jurgis", LastName="Šaulys", Email = "pastas@example.com", Password = "password", Blocked = true },
                 };
 
             return users;
