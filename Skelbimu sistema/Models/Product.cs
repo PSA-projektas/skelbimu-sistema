740685f3dcff0dd3230e9a47f54ae8cb38a624fd
MatasPal <102602807+MatasPal@users.noreply.github.com>
2024-04-10 21:41:21 +0300

    Update Product.cs

--- Skelbimu sistema/Models/Product.cs
+++ Skelbimu sistema/Models/Product.cs
@@ -4,23 +4,30 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Skelbimu_sistema.Models
 {
     public class Product
-	{
-		[Key]
-		public int Id { get; set; }
+    {
+        [Key]
+        public int Id { get; set; }
 
-		public string Name { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
 
-		public string Description { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
         public double Price { get; set; } = 0.0;
 
-		public string ImageUrl { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
 
-		public string StartDate { get; set; } 
+        public string StartDate { get; set; }
 
-		public string EndDate { get; set; }
+        public string EndDate { get; set; }
 
-		public Category Category { get; set; }
-    
-    }    
+        public Category Category { get; set; }
+
+        // foreign key to User
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        // Navigation property to access the related User entity (the seller)
+        //[ForeignKey("User")]
+        public virtual User Creator { get; set; }
+    }
+
 }
