a583f338b25d642e1a15626b80e2723ec63d014e
aivmic <aivarasnori@gmail.com>
2024-04-28 23:02:51 +0300

    beginning of paypal services

---
+++ Skelbimu sistema/Services/IPaypalServices.cs
@@ -0,0 +1,9 @@
+﻿using PayPal.Api;
+
+namespace Skelbimu_sistema.Services
+{
+    public interface IPaypalServices
+    {
+        Task<Payment> CreateOrderAsync(decimal amount, string returnUrl, string cancelUrl);
+    }
+}
